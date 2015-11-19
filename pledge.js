c48412a87f259f92f36a11466470e1908ddd17dc
Sam Narisi <snarisi@gmail.com>
2015-11-19 10:35:15 -0500

    Add solutions for ch1

--- pledge.js
+++ pledge.js
@@ -3,8 +3,34 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+var Deferral = function() {
+  
+}
 
+var $Promise = function() {
+  this.state = 'pending';
+}
 
+var defer = function() {
+  var newDeferral = new Deferral();
+  newDeferral.$promise = new $Promise();
+  
+  return newDeferral;
+}
+
+Deferral.prototype.resolve = function(value) {
+  if (this.$promise.state === 'pending') {
+    this.$promise.state = 'resolved';
+    this.$promise.value = value;
+  }
+}
+
+Deferral.prototype.reject = function(reason) {
+  if (this.$promise.state === 'pending') {
+    this.$promise.state = 'rejected';
+    this.$promise.value = reason;
+  }
+}
 
 
 
